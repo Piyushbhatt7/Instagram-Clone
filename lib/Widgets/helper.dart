dc4d7db51b5915dced0a52d3f129d4a62c2a280f
vengersmoon <vengersmon003@gmail.com>
2024-12-07 00:10:06 +0530

    insta

--- lib/Widgets/helper.dart
+++ lib/Widgets/helper.dart
@@ -7,38 +7,28 @@ class Helper {
       required bool tohide,
       required TextInputType textinputtype}) {
     return Container(
-       height: 44,
-       width: 343,
-       decoration: BoxDecoration(
-        color: Color(0XFF121212),
-        border: Border.all(color: Color(0xFFFFFFFF)),
-        borderRadius: BorderRadius.circular(5)
-       ),
-
-       child: TextField(
-      controller: controller,
-      obscureText: tohide,
-      keyboardType: textinputtype,
-      decoration: InputDecoration(
-          hintText: text,
-          hintStyle: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.normal,
-              color: Color(0XFFFFFFFF)
-              
-              ),
-
-              border: InputBorder.none
-              
-              ),
-    ),
+      height: 44,
+      width: 343,
+      decoration: BoxDecoration(
+          color: Color(0XFF121212),
+          border: Border.all(color: Color(0xFFFFFFFF)),
+          borderRadius: BorderRadius.circular(5)),
+      child: TextField(
+        controller: controller,
+        obscureText: tohide,
+        keyboardType: textinputtype,
+        decoration: InputDecoration(
+            hintText: text,
+            hintStyle: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.normal,
+                color: Color(0XFFFFFFFF)),
+            border: InputBorder.none),
+      ),
     );
-
   }
 
-  static CustomImage({required String imgurl}){
-
-    return Image.asset("assets/images/instlogo.png");
+  static CustomImage({required String imgurl}) {
+    return Image.asset("assets/images/$imgurl");
   }
-
 }
