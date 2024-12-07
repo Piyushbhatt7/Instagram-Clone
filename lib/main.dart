f60015f0cec649bfdaac530434bbc4b8c9f97229
vengersmoon <vengersmon003@gmail.com>
2024-12-07 12:30:28 +0530

    SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Instagram',
-      theme: ThemeData(
-     
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: ThemeData.dark(),
       home: SplashScreen(),
     );
   }
