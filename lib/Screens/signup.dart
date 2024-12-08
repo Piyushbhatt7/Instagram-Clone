843b2c5b606727d32a168c8ab929557e60d8786b
vengersmoon <vengersmon003@gmail.com>
2024-12-08 14:13:13 +0530

    SizedBox(height: 5,),

--- lib/Screens/signup.dart
+++ lib/Screens/signup.dart
@@ -6,29 +6,34 @@ class SignUp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     TextEditingController emailController = TextEditingController();
     TextEditingController passwordController = TextEditingController();
     TextEditingController usernameController = TextEditingController();
-   
-    return Scaffold(
 
+    return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Helper.CustomImage(imgurl: 'instlogo.png'),
-
-            SizedBox(height: 10,),
-
-            Helper.CustomTextField(controller: emailController, text: "Email", tohide: false),
-            SizedBox(height: 10,),
-            Helper.CustomTextField(controller: passwordController, text: "Password", tohide: true),
-             SizedBox(height: 10,),
-            Helper.CustomTextField(controller: usernameController, text: "Username", tohide: false)
+            Helper.CustomImage(imgurl: 'instatext.png'),
+            SizedBox(
+              height: 30,
+            ),
+            Helper.CustomTextField(
+                controller: emailController, text: "Email", tohide: false),
+            SizedBox(
+              height: 10,
+            ),
+            Helper.CustomTextField(
+                controller: passwordController, text: "Password", tohide: true),
+            SizedBox(
+              height: 10,
+            ),
+            Helper.CustomTextField(
+                controller: usernameController, text: "Username", tohide: false)
           ],
         ),
       ),
     );
   }
-} 
+}
