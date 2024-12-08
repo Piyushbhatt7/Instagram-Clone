af420069dedc63747cac8ce41de6a2e1c98a9138
vengersmoon <vengersmon003@gmail.com>
2024-12-08 17:56:10 +0530

    insta

--- lib/Screens/bottomnav/bootomnav.dart
+++ lib/Screens/bottomnav/bootomnav.dart
@@ -10,6 +10,8 @@ class BootomNav extends StatefulWidget {
 class _BootomNavState extends State<BootomNav> {
 
   int curretIndex = 0;
+
+  List<Widget>pages = 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
