bf0ce099f3840ca42d9ee0edb22b8478956df7cc
vengersmoon <vengersmon003@gmail.com>
2024-12-08 17:54:38 +0530

    insta

---
+++ lib/Screens/bottomnav/bootomnav.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BootomNav extends StatefulWidget {
+  const BootomNav({super.key});
+
+  @override
+  State<BootomNav> createState() => _BootomNavState();
+}
+
+class _BootomNavState extends State<BootomNav> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
