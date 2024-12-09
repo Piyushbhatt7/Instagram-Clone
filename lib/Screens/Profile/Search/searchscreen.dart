f77aaeec190b228cc3ada0636dd6612e6f546f98
vengersmoon <vengersmon003@gmail.com>
2024-12-09 11:18:47 +0530

    insta

--- lib/Screens/Profile/Search/searchscreen.dart
+++ lib/Screens/Profile/Search/searchscreen.dart
@@ -9,24 +9,28 @@ class SearchScreen extends StatelessWidget {
     TextEditingController searchContrller = TextEditingController();
     return  Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           SizedBox(height: 50,),
-            Container(
-              height: 36,
-              width: 327,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xFF262626)
-              ),
-              child: TextField(
-                controller: searchContrller,
-                decoration: InputDecoration(
-                  hintText: "Search",
-                  hintStyle: TextStyle(fontSize: 16, color: Color(0XFF8E8E93)),
-                  border: InputBorder.none
+           
+            Row(
+              children: [
+                Container(
+                  height: 36,
+                  width: 327,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xFF262626)
+                  ),
+                  child: TextField(
+                    controller: searchContrller,
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      hintStyle: TextStyle(fontSize: 16, color: Color(0XFF8E8E93)),
+                      border: InputBorder.none
+                    ),
+                  ),
                 ),
-              ),
+              ],
             )
         ],
       )
