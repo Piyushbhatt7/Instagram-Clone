53daace70d453a7a2694e1a707198c1f58e518f8
vengersmoon <vengersmon003@gmail.com>
2024-12-09 15:44:58 +0530

    https://media.cnn.com/api/v1/images/stellar/prod/231209082454-02-cristiano-ronaldo-al-nassr-1208.jpg?c=original

--- lib/Screens/Profile/Search/searchscreen.dart
+++ lib/Screens/Profile/Search/searchscreen.dart
@@ -10,7 +10,7 @@ class SearchScreen extends StatelessWidget {
 
     var arrContent = [
        {
-          "img": " https://s.ndtvimg.com/images/content/2014/aug/806/cristiano-ronaldo-real-madrid-super-cup.jpg"
+          "img": "https://media.cnn.com/api/v1/images/stellar/prod/231209082454-02-cristiano-ronaldo-al-nassr-1208.jpg?c=original"
        },
 
          {
@@ -47,13 +47,13 @@ class SearchScreen extends StatelessWidget {
                 SizedBox(width: 10,),
                 Container(
                   height: 36,
-                  width: 340,
+                  width: 285,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
                     color: Color(0xFF262626)
                   ),
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 15.0),
+                    padding: const EdgeInsets.only(left: 10.0),
                     child: TextField(
                       controller: searchContrller,
                       decoration: InputDecoration(
@@ -66,26 +66,27 @@ class SearchScreen extends StatelessWidget {
                   ),
                 ),
                 
-                SizedBox(width: 20.0,),
+                SizedBox(width: 10.0,),
                 Helper.CustomImage(imgurl: "Live.png"),
-
+      
                
               ],
             ),
-
+      
             SizedBox(height: 5.0,),
             
              Row(
+                         //  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    SizedBox(width: 15.0,),
+                    SizedBox(width: 10.0,),
                     Container(
                       height: 32,
-                      width: 75,
+                      width: 72,
                       decoration: BoxDecoration(
                          border: Border.all(color: Colors.white10),
                          borderRadius: BorderRadius.circular(3)
                       ),
-
+             
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -95,38 +96,40 @@ class SearchScreen extends StatelessWidget {
                         ],
                       ),
                     ),
-
+             
                     SizedBox(width: 5.0,),
-
+             
+                     Expanded(
+                       child: Container(
+                        height: 32,
+                        width: 75,
+                        decoration: BoxDecoration(
+                           border: Border.all(color: Colors.white10),
+                           borderRadius: BorderRadius.circular(3)
+                        ),
+                                    
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Helper.CustomImage(imgurl: "shop.png"),
+                            SizedBox(width: 5.0,),
+                            Text("Shop", style: TextStyle(fontSize: 14, color: Color(0XFFF9F9F9)),)
+                          ],
+                        ),
+                                           ),
+                     ),
+             
+                    SizedBox(width: 5.0,),
+             
+             
                      Container(
                       height: 32,
-                      width: 75,
+                      width: 68,
                       decoration: BoxDecoration(
                          border: Border.all(color: Colors.white10),
                          borderRadius: BorderRadius.circular(3)
                       ),
-
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Helper.CustomImage(imgurl: "shop.png"),
-                          SizedBox(width: 5.0,),
-                          Text("Shop", style: TextStyle(fontSize: 14, color: Color(0XFFF9F9F9)),)
-                        ],
-                      ),
-                    ),
-
-                    SizedBox(width: 5.0,),
-
-
-                     Container(
-                      height: 32,
-                      width: 75,
-                      decoration: BoxDecoration(
-                         border: Border.all(color: Colors.white10),
-                         borderRadius: BorderRadius.circular(3)
-                      ),
-
+             
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -136,19 +139,19 @@ class SearchScreen extends StatelessWidget {
                         ],
                       ),
                     ),
-
+             
                     SizedBox(width: 5.0,),
-
-
-
+             
+             
+             
                      Container(
                       height: 32,
-                      width: 75,
+                      width: 68,
                       decoration: BoxDecoration(
                          border: Border.all(color: Colors.white10),
                          borderRadius: BorderRadius.circular(3)
                       ),
-
+             
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -158,17 +161,17 @@ class SearchScreen extends StatelessWidget {
                         ],
                       ),
                     ),
-
+             
                     SizedBox(width: 5.0,),
-
+             
                      Container(
                       height: 32,
-                      width: 53,
+                      width: 50,
                       decoration: BoxDecoration(
                          border: Border.all(color: Colors.white10),
                          borderRadius: BorderRadius.circular(3)
                       ),
-
+             
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -178,18 +181,20 @@ class SearchScreen extends StatelessWidget {
                         ],
                       ),
                     ),
-
+             
                     
                   ],
                 ),
-
+                //SizedBox(height: 10,),
+      
                 Expanded(
                   child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3), itemBuilder: (context, index){
                   
                     return Container(
                       clipBehavior: Clip.antiAlias,
-                      height: 124,
+                      height: 100,
                       width: 124,
+                      decoration: BoxDecoration(),
                       child: Image.network(arrContent[index]["img"].toString(),fit: BoxFit.cover,),
                     );
                   }, itemCount: arrContent.length,
