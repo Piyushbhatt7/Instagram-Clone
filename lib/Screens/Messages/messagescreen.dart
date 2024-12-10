8819b31a3970a411fc83f394a4157e9703bd8980
vengersmoon <vengersmon003@gmail.com>
2024-12-10 15:24:01 +0530

    d

--- lib/Screens/Messages/messagescreen.dart
+++ lib/Screens/Messages/messagescreen.dart
@@ -12,7 +12,7 @@ class MessageScreen extends StatelessWidget {
           leading: IconButton(onPressed: (){
 
           }, icon: Icon(CupertinoIcons.back)),
-          title: Text("Piyush",style: TextStyle(fontSize: 16, color: Color(0XFF9F9F9F9)),),
+          title: Text("piyushbhatt",style: TextStyle(fontSize: 16, color: Color(0XFF9F9F9F9)),),
         ),
     );
   }
