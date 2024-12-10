4d9883815c66f65fa80285457a6a1451dc835dfd
vengersmoon <vengersmon003@gmail.com>
2024-12-10 22:38:29 +0530

    hh

--- lib/Screens/Messages/messagescreen.dart
+++ lib/Screens/Messages/messagescreen.dart
@@ -53,6 +53,54 @@ class MessageScreen extends StatelessWidget {
                              "img": "pic7.png",
                              "name": "maxjacobson",
                               "lastmsg": "See you on the next meeting"
+                   },
+
+                    {
+                        "img": "pi.png",
+                        "name": "josuha_ima",
+                        "lastmsg": "Have a great day"
+                   },
+
+                   {
+                         "img": "pic1.png",
+                         "name": "karenne",
+                          "lastmsg": "I heard you are in London"
+                   },
+
+                   {
+                          "img": "pic2.png",
+                          "name": "andreww_",
+                           "lastmsg": "Hii"
+                   },
+
+                   {
+                           "img": "pic3.png",
+                           "name": "martini_rond",
+                            "lastmsg": "Good morning"
+                   },
+
+                   {
+                           "img": "pic4.png",
+                           "name": "kiero_do",
+                            "lastmsg": "Today's wheather is nice"
+                   },
+
+                   {
+                            "img": "pic5.png",
+                            "name": "jannie.franco",
+                             "lastmsg": "Like a story"
+                   },
+
+                   {
+                            "img": "pic6.png",
+                            "name": "m_humphrey",
+                             "lastmsg": "Sounds good 😂😂"
+                   },
+
+                   {
+                             "img": "pic7.png",
+                             "name": "maxjacobson",
+                              "lastmsg": "See you on the next meeting"
                    }
   ];
   //const MessageScreen({super.key});
@@ -116,8 +164,8 @@ class MessageScreen extends StatelessWidget {
                         radius: 25,
                          backgroundImage: AssetImage("assets/images/${arrContent[index]["img"].toString()}"),
                       ),
-                       title: Text(arrContent[index]["name"].toString(),style: TextStyle(fontSize: 13),),
-                       subtitle: Text(arrContent[index]["lastmsg"].toString()),
+                       title: Text(arrContent[index]["name"].toString(),style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),),
+                       subtitle: Text(arrContent[index]["lastmsg"].toString(),style: TextStyle(fontSize: 13, color: Colors.grey),),
                        trailing: Icon(CupertinoIcons.camera),
                
                   );
