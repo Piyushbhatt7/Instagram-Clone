6d6161b5dce48b25edccca71f7938cbf9650af84
vengersmoon <vengersmon003@gmail.com>
2024-12-10 14:25:28 +0530

    h

--- lib/Screens/Messages/messagescreen.dart
+++ lib/Screens/Messages/messagescreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MessageScreen extends StatelessWidget {
@@ -6,7 +7,13 @@ class MessageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+        
+        appBar: AppBar(
+          leading: IconButton(onPressed: (){
+
+          }, icon: Icon(CupertinoIcons.back)),
+          title: Text("Piyush",style: TextStyle(fontSize: 16, color: Color(0XFF9F9F9F9)),),
+        ),
     );
   }
 }
