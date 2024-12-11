f371a31587ed1af540b11966977025b08eeb34ec
vengersmoon <vengersmon003@gmail.com>
2024-12-11 14:55:20 +0530

    f

--- lib/Screens/Profile/post.dart
+++ lib/Screens/Profile/post.dart
@@ -9,7 +9,20 @@ class Post extends StatelessWidget {
     {
        "img": "https://louisphilippe.abfrl.in/blog/wp-content/uploads/2024/03/Tips-for-Choosing-the-Right-Mens-Formal-Wear-Outfits.jpg"
     },
+ 
+  {
 
+      "img": "https://icm.aexp-static.com/Internet/internationalcardshop/en_sg/images/cards/gold-card.png"
+  },
+
+     {
+
+         "img": "https://www.akc.org/wp-content/uploads/2017/11/Siberian-Husky-standing-outdoors-in-the-winter.jpg"
+     },
+
+     {
+         "img": "https://images.squarespace-cdn.com/content/v1/6631ae2b40e7b9085b2a57da/7a6efbe2-ac68-4221-9b4e-2ab7aad7a9c5/ESH_7498-Edit.jpg"
+     }
 
   ];
 
