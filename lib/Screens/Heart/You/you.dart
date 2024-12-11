b1e3e3c7778cfcd6b86e9978a91874e1b89f5f0a
vengersmoon <vengersmon003@gmail.com>
2024-12-11 23:08:59 +0530

    d

--- lib/Screens/Heart/You/you.dart
+++ lib/Screens/Heart/You/you.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/Widgets/helper.dart';
 
 class YouScreen extends StatelessWidget {
   const YouScreen({super.key});
@@ -6,22 +7,32 @@ class YouScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Container(
-          height: 40,
-          width: double.infinity,
-          color: Colors.black,
-          child: Column(
-            children: [
-              Text(
-                "Follow Request",
-                style: TextStyle(fontSize: 15, color: Color(0XFFF9F9F9)),
-              ),
-            ],
+      body: Column(
+        children: [
+          Container(
+            height: 48,
+            width: double.infinity,
+            color: Colors.black,
+
+            child: Padding(
+              padding: const EdgeInsets.only(top: 15.0, left: 20),
+              child: Text("Follow Request",style: TextStyle(fontSize: 15, color: Color(0XFFF9F9F9)),),
+            ),
           ),
-        )
-      ],
-    ));
+
+          Container(
+            height: 106,
+            width: double.infinity,
+            color: Colors.black,
+
+            child: Column(
+              children: [
+                Helper.CustomImage(imgurl: imgurl)
+              ],
+            ),
+          )
+        ],
+      )
+    );
   }
 }
