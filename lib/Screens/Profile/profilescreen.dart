cd54acd3b19aee8c102603472b2f411960696055
vengersmoon <vengersmon003@gmail.com>
2024-12-11 11:14:44 +0530

    ff

--- lib/Screens/Profile/profilescreen.dart
+++ lib/Screens/Profile/profilescreen.dart
@@ -5,12 +5,20 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: AppBar(
-         title: Text("piyushbhatt"),
-         centerTitle: true,
-        
-      )
-    );
+    return Scaffold(
+        body: AppBar(
+      title: Row(
+        children: [
+          Text(
+            "piyushbhatt",
+            style: TextStyle(
+                fontSize: 16,
+                color: Color(0XFFF9F9F9),
+                fontWeight: FontWeight.w500),
+          ),
+        ],
+      ),
+      centerTitle: true,
+    ));
   }
 }
