ce5c0967b1477c3ae6309502bc48f40ed9d09e1f
vengersmoon <vengersmon003@gmail.com>
2024-12-11 14:51:54 +0530

    hjh

--- lib/Screens/Profile/post.dart
+++ lib/Screens/Profile/post.dart
@@ -2,12 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Post extends StatelessWidget {
-  const Post({super.key});
+ // const Post({super.key});
+
+  var arrContent = [
+
+    {
+       "img": "https://louisphilippe.abfrl.in/blog/wp-content/uploads/2024/03/Tips-for-Choosing-the-Right-Mens-Formal-Wear-Outfits.jpg"
+    },
+
+
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Post"),
+     
+
+     body: GridView(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+     children: [
+
+     ],),
+    
+    
     );
   }
 }
