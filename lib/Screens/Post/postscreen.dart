8340629a85f19f8189f8a53d3321e99caec2ec80
vengersmoon <vengersmon003@gmail.com>
2024-12-12 23:02:39 +0530

    f

--- lib/Screens/Post/postscreen.dart
+++ lib/Screens/Post/postscreen.dart
@@ -5,7 +5,7 @@ class PostScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Center(
         child: Text('Post screen'),
       ),
