189781c7c93437dd68d505f4903cd0a9d4147cb3
vengersmoon <vengersmon003@gmail.com>
2024-12-12 22:57:52 +0530

    d

--- lib/Screens/bottomnav/bootomnav.dart
+++ lib/Screens/bottomnav/bootomnav.dart
@@ -9,7 +9,7 @@ import 'package:instagram_clone/Screens/Profile/profilescreen.dart';
 
 class BootomNav extends StatefulWidget {  
   const BootomNav({super.key});  
- 
+  
   @override 
   State<BootomNav> createState() => _BootomNavState();
 }
