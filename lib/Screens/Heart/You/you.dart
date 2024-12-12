6dd744ef338e261a1a41a51774adab3348b2f087
vengersmoon <vengersmon003@gmail.com>
2024-12-12 23:01:08 +0530

    d

--- lib/Screens/Heart/You/you.dart
+++ lib/Screens/Heart/You/you.dart
@@ -8,8 +8,8 @@ class YouScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
-          child: Column(
-                children: [
+      child: Column(
+        children: [
           Container(
             height: 48,
             width: double.infinity,
@@ -28,7 +28,7 @@ class YouScreen extends StatelessWidget {
           Container(
             height: 110,
             width: double.infinity,
-           // color: Colors.black,
+            // color: Colors.black,
             child: Column(
               children: [
                 SizedBox(
@@ -45,11 +45,15 @@ class YouScreen extends StatelessWidget {
                     ),
                   ],
                 ),
-                SizedBox(height: 10,),
+                SizedBox(
+                  height: 10,
+                ),
                 Padding(
                   padding: const EdgeInsets.only(left: 5.0),
                   child: ListTile(
-                    leading: Helper.CustomImage(imgurl: "ove.png",),
+                    leading: Helper.CustomImage(
+                      imgurl: "ove.png",
+                    ),
                     title: Text(
                       "Karenne liked you photo. 1h",
                       style: TextStyle(fontSize: 12),
@@ -57,130 +61,129 @@ class YouScreen extends StatelessWidget {
                     trailing: Helper.CustomImage(imgurl: "rec.png"),
                   ),
                 ),
-          
               ],
             ),
           ),
-          
-           SizedBox(height: 10,),
-          
-           Row(
+          SizedBox(
+            height: 10,
+          ),
+          Row(
             children: [
-              SizedBox(width: 20,),
-              Text("Today",style: TextStyle(fontSize: 15),),
-            ],
-           ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 5.0),
-                  child: ListTile(
-                    leading: Helper.CustomImage(imgurl: "pro1.png",),
-                    title: Text(
-                      "jiss_o liked you photo. 1h",
-                      style: TextStyle(fontSize: 12),
-                    ),
-                    trailing: Helper.CustomImage(imgurl: "rec.png"),
-                  ),
-                ),
-          
-                 SizedBox(height: 10,),
-          
-                  Padding(
-                  padding: const EdgeInsets.only(left: 5.0),
-                  child: ListTile(
-                    leading: Helper.CustomImage(imgurl: "pro2.png",),
-                    title: Text(
-                      "emma_08 Comment on your photo. 1h",
-                      style: TextStyle(fontSize: 12),
-                    ),
-                    trailing: Helper.CustomImage(imgurl: "rec.png"),
-                  ),
-                ),
-          
-          
-                
-           SizedBox(height: 10,),
-          
-           Row(
-            children: [
-              SizedBox(width: 20,),
-              Text("This week",style: TextStyle(fontSize: 15),),
-            ],
-           ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 5.0),
-                  child: ListTile(
-                    leading: Helper.CustomImage(imgurl: "Profiles.png",),
-                    title: Text(
-                      "Sam liked you photo. 1h",
-                      style: TextStyle(fontSize: 12),
-                    ),
-                    trailing: Helper.CustomImage(imgurl: "rec.png"),
-                  ),
-                ),
-          
-          
-          
-                
-           SizedBox(height: 10,),
-          
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: ListTile(
-                    leading: Helper.CustomImage(imgurl: "pro3.png",),
-                    title: Text(
-                      "zayn_mak started following you. 4d",
-                      style: TextStyle(fontSize: 12),
-                    ),
-                    trailing: Container(
-                       width: 90,
-                       height: 30,
-                       decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.white24
-                        ),
-          
-                        borderRadius: BorderRadius.circular(3)
-                       ),
-           
-                       child: Center(
-                        child: Text("Message"),
-                       ),
-                    )
-                  ),
-                ),
-          
-          
-                
-          SizedBox(height: 10,),
-          
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: ListTile(
-                    leading: Helper.CustomImage(imgurl: "pro5.png",),
-                    title: Text(
-                      "jackson started following you. 4d",
-                      style: TextStyle(fontSize: 12),
-                    ),
-                    trailing: Container(
-                       width: 90,
-                       height: 30,
-                       decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.white24
-                        ),
-          
-                        borderRadius: BorderRadius.circular(3)
-                       ),
-           
-                       child: Center(
-                        child: Text("Message"),
-                       ),
-                    )
-                  ),
-                ),
-                ],
+              SizedBox(
+                width: 20,
               ),
-        )
-    );
+              Text(
+                "Today",
+                style: TextStyle(fontSize: 15),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 5.0),
+            child: ListTile(
+              leading: Helper.CustomImage(
+                imgurl: "pro1.png",
+              ),
+              title: Text(
+                "jiss_o liked you photo. 1h",
+                style: TextStyle(fontSize: 12),
+              ),
+              trailing: Helper.CustomImage(imgurl: "rec.png"),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 5.0),
+            child: ListTile(
+              leading: Helper.CustomImage(
+                imgurl: "pro2.png",
+              ),
+              title: Text(
+                "emma_08 Comment on your photo. 1h",
+                style: TextStyle(fontSize: 12),
+              ),
+              trailing: Helper.CustomImage(imgurl: "rec.png"),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              Text(
+                "This week",
+                style: TextStyle(fontSize: 15),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 5.0),
+            child: ListTile(
+              leading: Helper.CustomImage(
+                imgurl: "Profiles.png",
+              ),
+              title: Text(
+                "Sam liked you photo. 1h",
+                style: TextStyle(fontSize: 12),
+              ),
+              trailing: Helper.CustomImage(imgurl: "rec.png"),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: ListTile(
+                leading: Helper.CustomImage(
+                  imgurl: "pro3.png",
+                ),
+                title: Text(
+                  "zayn_mak started following you. 4d",
+                  style: TextStyle(fontSize: 12),
+                ),
+                trailing: Container(
+                  width: 90,
+                  height: 30,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white24),
+                      borderRadius: BorderRadius.circular(3)),
+                  child: Center(
+                    child: Text("Message"),
+                  ),
+                )),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: ListTile(
+                leading: Helper.CustomImage(
+                  imgurl: "pro5.png",
+                ),
+                title: Text(
+                  "jackson started following you. 4d",
+                  style: TextStyle(fontSize: 12),
+                ),
+                trailing: Container(
+                  width: 90,
+                  height: 30,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white24),
+                      borderRadius: BorderRadius.circular(3)),
+                  child: Center(
+                    child: Text("Message"),
+                  ),
+                )),
+          ),
+        ],
+      ),
+    ));
   }
 }
